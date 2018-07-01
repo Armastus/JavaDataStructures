40e55a4c4ebd8e55ef92cdba5a98db7ad620483c
Armastus <david.o.bustillos@gmail.com>
2018-07-01 16:31:17 -0700

    commit

---
+++ src/HackerRankInterviewPrep/BubbleSort/Solution.java
@@ -0,0 +1,16 @@
+package HackerRankInterviewPrep.BubbleSort;
+
+public class Solution {
+    public static void bubbleSort (int[] arr) {
+        boolean isSorted = false;
+        arrLength = arr.length - 1;
+
+        System.out.print(isSorted);
+//        while(!isSorted)
+    }
+
+    public static void main(String[] args) {
+        int[] blah = {1, 2};
+        bubbleSort(blah);
+    }
+}
