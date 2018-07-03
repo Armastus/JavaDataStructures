f5e49b7e3c4173ac4b7576ed2dcc10ab8228fb75
david <david.o.bustillos@gmail.com>
2018-07-02 22:45:13 -0700

    setting up maximum element

--- src/HackerRankCrackingTheCode/BSTExample/Solution.java
+++ src/HackerRankCrackingTheCode/BSTExample/Solution.java
@@ -1,5 +1,11 @@
 package HackerRankCrackingTheCode.BSTExample;
 
 public class Solution {
-
+    public static void main(String[] args) {
+        Node node = new Node(3);
+        node.insert(2);
+        node.insert(1);
+        node.insert(7);
+        node.insert(5);
+    }
 }
