bcfc0f2b80437a81ad5a647d58996d9c94894b4e
david <david.o.bustillos@gmail.com>
2018-07-04 18:48:33 -0700

    setting up maximum element

---
+++ src/HackerRankInterviewPrep/ArrayGreedyAlgorithm/Solution.java
@@ -0,0 +1,35 @@
+package HackerRankInterviewPrep.ArrayGreedyAlgorithm;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the minimumAbsoluteDifference function below.
+    static int minimumAbsoluteDifference(int[] arr) {
+        Arrays.sort(arr);
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i<arr.length-1;i++){
+            int sum  = Math.abs(arr[i]- arr[i+1]);
+            if(min >(sum))
+                min = sum;
+        }
+        return min;
+    }
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] arr = {-59, -36, -13, 1, -53, -92, -2, -96, -54, 75};
+
+
+        int result = minimumAbsoluteDifference(arr);
+
+        System.out.println(result);
+    }
+}
+
